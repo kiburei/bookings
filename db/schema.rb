3d38e13a98f135b28ff48bd4c2fa1ca126ea67e6
kiburei <gooddeeds17@gmil.com>
2018-08-27 12:29:07 +0300

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_122809) do
+ActiveRecord::Schema.define(version: 2018_08_27_092832) do
 
   create_table "bookings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "checkin"
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(version: 2018_08_24_122809) do
     t.bigint "room_type_id"
     t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
     t.index ["room_type_id"], name: "index_rooms_on_room_type_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bookings", "hotels"
